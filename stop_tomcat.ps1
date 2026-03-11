f3852bb64f7841aa7a7ff601f599fd85e4c1ccf6
sjrnfl13 <sjrnfl13@tobesoft.com>
2026-03-11 09:34:53 +0900

    gitignore 내용변경

--- stop_tomcat.ps1
+++
@@ -1,7 +0,0 @@
-$CATALINA_HOME = "D:\git_prj\REQM\apache-tomcat-9.0.89"
-$env:CATALINA_HOME = $CATALINA_HOME
-$env:JAVA_HOME = "C:\microsoft-jdk-21.0.9-windows-x64\jdk-21.0.9+10"
-
-Write-Host "Stopping Tomcat server..." -ForegroundColor Yellow
-Start-Process -FilePath "$CATALINA_HOME\bin\shutdown.bat" -WorkingDirectory "$CATALINA_HOME\bin" -Wait
-Write-Host "Tomcat server stopped." -ForegroundColor Green
