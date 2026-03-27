2dbfc07e914cb039ca8abdc2c9d5fc721dff370d
sjrnfl13 <sjrnfl13@tobesoft.com>
2026-03-27 16:32:05 +0900

    bat 파일 변경

--- tools/start_tomcat.bat
+++ tools/start_tomcat.bat
@@ -6,7 +6,7 @@ setlocal
 set "CATALINA_HOME=%~dp0..\apache-tomcat-9.0.89"
 REM ===== 자바 경로 설정 (시스템 변수 기반 명시적 설정) =====
 set "JAVA_HOME=C:\microsoft-jdk-21.0.9-windows-x64\jdk-21.0.9+10"
-set "JRE_HOME=C:\java-1.8.0-openjdk-1.8.0.332-1.b09.ojdkbuild.windows.x86_64\jre"
+set "JRE_HOME=C:\microsoft-jdk-21.0.9-windows-x64\jdk-21.0.9+10"
 
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 
