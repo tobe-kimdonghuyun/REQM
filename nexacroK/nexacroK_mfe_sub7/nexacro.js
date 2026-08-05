f0d19be2e07354bc06a0ef922755e0bc6360e9c5
sjrnfl13 <sjrnfl13@tobesoft.com>
2026-08-05 09:05:44 +0900

    금일아침업데이트

---
+++ nexacroK/nexacroK_mfe_sub7/nexacro.js
@@ -0,0 +1,7 @@
+import { nexacro, createApp, system, trace } from "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/framework/Framework.js";
+import "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/component/CompBase.js";
+import "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/component/ComComp.js";
+import "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/component/Grid.js";
+import "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/component/ListView.js";
+import "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/component/DeviceAPI.js";
+import "http://172.10.12.45:9091/nexacroK/nexacroK_NexaPortal_test_03/ nexacrolib/component/MobileComp.js";
